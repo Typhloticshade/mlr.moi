ad527e1a2b3b6d30c506ca86a30558b6bec9ef3e
Typhloticshde <katsuyori@gmail.com>
2020-08-20 00:54:37 -0700

    Updates

---
+++ precache-manifest.909562ff8fb7d6c0ad377f7de8f53f71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7feaa0a67d36b36f858ced152ef2304",
+    "url": "/mlr.moi/index.html"
+  },
+  {
+    "revision": "ae2ce265a0fbd0463276",
+    "url": "/mlr.moi/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4d50481a0eae2d9097e3",
+    "url": "/mlr.moi/static/js/2.e242e974.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mlr.moi/static/js/2.e242e974.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae2ce265a0fbd0463276",
+    "url": "/mlr.moi/static/js/main.04fc38a4.chunk.js"
+  },
+  {
+    "revision": "db227682da8dc7610333",
+    "url": "/mlr.moi/static/js/runtime-main.907227f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mlr.moi/static/media/logo.5d5d9eef.svg"
+  }
+]);
